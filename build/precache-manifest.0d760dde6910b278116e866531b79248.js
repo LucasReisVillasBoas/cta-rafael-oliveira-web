47b15fceeb312d536a69263cca66f049fbd00030
LucasReisVillasBoas <lucas.boas@rubcube.com>
2025-04-13 20:33:15 -0300

    fix: Update homepage value

---
+++ build/precache-manifest.0d760dde6910b278116e866531b79248.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9decb391d3f5c4ee4e539394be4dca26",
+    "url": "/cta-rafael-oliveira-web/index.html"
+  },
+  {
+    "revision": "1e12cde74da43dbdb2a8",
+    "url": "/cta-rafael-oliveira-web/static/css/main.1ca73cf6.chunk.css"
+  },
+  {
+    "revision": "cb170ed7f435afc85587",
+    "url": "/cta-rafael-oliveira-web/static/js/2.27505fc1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/cta-rafael-oliveira-web/static/js/2.27505fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e12cde74da43dbdb2a8",
+    "url": "/cta-rafael-oliveira-web/static/js/main.3b924004.chunk.js"
+  },
+  {
+    "revision": "33b5d20db1ca38b971ed",
+    "url": "/cta-rafael-oliveira-web/static/js/runtime-main.be1f0115.js"
+  },
+  {
+    "revision": "4280ec4d35ac84efae8aeaf09645dab9",
+    "url": "/cta-rafael-oliveira-web/static/media/bg.4280ec4d.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/cta-rafael-oliveira-web/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/cta-rafael-oliveira-web/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/cta-rafael-oliveira-web/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/cta-rafael-oliveira-web/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/cta-rafael-oliveira-web/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "8b6364f86c57975e2e1d7bcfbe9d1ac8",
+    "url": "/cta-rafael-oliveira-web/static/media/rafa1.8b6364f8.png"
+  },
+  {
+    "revision": "2a08432fe7106981354d831d8d8a100c",
+    "url": "/cta-rafael-oliveira-web/static/media/rafa2.2a08432f.png"
+  }
+]);
