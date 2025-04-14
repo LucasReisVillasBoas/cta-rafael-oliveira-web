0a547e81447c03775b9b676743dc22a5afb5156d
LucasReisVillasBoas <lucas.boas@rubcube.com>
2025-04-13 21:27:24 -0300

    Update Build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cta-rafael-oliveira-web/precache-manifest.217d7f7879a4d97f2a8747c1bf656d02.js"
+  "./precache-manifest.c4c7450502babc209ef1c40cde920994.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cta-rafael-oliveira-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
